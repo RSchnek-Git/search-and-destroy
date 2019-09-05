eea306795b42054f844187ef1ffeeefd0e5c9fac
Robert Schnek <robert.schnek@gmail.com>
2019-09-05 16:08:20 -0400

    solved the first one! yay

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,36 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let remainingArray = []
+	if(array.length <= 2){
+		if(array[0] === target || array[1] === target){
+			console.log("array[0]:", array[0])
+			console.log("array[1]:", array[1])
+			console.log("array:", array)
+			return true
+		} else {
+			console.log("FALSE!!!!")
+			return false
+		}
+	}
+	// let i = 0
+	// let j = array.length-1
+	// while( i <= j && array.length > 1 ){
+	// 	if (array[i] === target || array[j] === target){
+	// 		return true
+	// 	} else {
+	// 		i++
+	// 		j--
+	// 	}
+	// }
+	// return false
+	let median = Math.floor(array.length/2)
+	if(array[median]===target){
+		return true
+	} else {
+		remainingArray = target < array[median] ? array.slice(0,median) : array.slice(median)
+		return binarySearch(remainingArray, target);
+	}
 };
 
 /*
--- binary-search/binary-search.spec.js
+++ binary-search/binary-search.spec.js
@@ -5,6 +5,7 @@ const binarySearch = require('./binary-search');
 const test1 = [1,3,4,5,10,15,20,22,23,98,1000];
 const test2 = [-3,-2,0, 9, 10, 11, 12, 15, 17, 18, 19, 20];
 const test3 = [-0.5, 0.03, 2.6, 3, 3.5, 3.777777, 5];
+const test4 = [0,1,4,5]
 
 
 describe('Binary Search', () => {
@@ -13,6 +14,7 @@ describe('Binary Search', () => {
 		expect(binarySearch(test2, -3)).to.equal(true);
 		expect(binarySearch(test2, 20)).to.equal(true);
 		expect(binarySearch(test3, 2.6)).to.equal(true);
+		expect(binarySearch(test4, 1)).to.equal(true);
 	});
 
 	it('returns false if target value is not in array', () => {
