3099107ac8a8e3c78fcbf0c87a8ae2d76008558b
Robert Schnek <robert.schnek@gmail.com>
2019-09-06 13:34:50 -0400

    Fixed minJumps :)

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,30 @@
 
 // Complete this algo
 const minJumps = arr => {
-
+    let numJumps = 1
+    while(true){
+    let start = arr[0]
+    let maxSum = 0;
+    let currentSum = 0;
+    let correctIdx = 0;
+        if (start >= arr.length-1){ //if we get to the end from the start value
+            return numJumps
+        }
+        for(let i = 0; i <= start; i++){ // if we get to the end based off jumping to the next number after start
+            if(i+arr[i] >= arr.length-1){ //if maxSum < currentSum, idx = i USE THIS
+                numJumps++
+                return numJumps
+            } else {
+                currentSum = i+arr[i]
+                if (maxSum < currentSum){
+                    maxSum = currentSum
+                    correctIdx = i
+                }
+            }
+        }
+        arr = arr.slice(correctIdx) //we didn't reach the end yet, but we have to "jump" to next point, then delete the rest of the array behind us
+        numJumps++
+    }
 };
 
 module.exports = minJumps
